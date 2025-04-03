5f02b299278fc516ad41ad4487596806c06a4f7c
diamond <chokwalakishan@gmail.com>
2025-04-03 12:22:58 +0530

    Fix backend deployment settings

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from './Components/ThemeContext';
 import Login from "./Components/Login";
 import Signup from "./Components/Signup";
+import Dashboard from './Components/Dashboard';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Routes>
             <Route index element={<Login />} />
             <Route path="/Signup" element={<Signup />} />
+            <Route path="/Dashboard" element={<Dashboard />} />
           </Routes>
         </HashRouter>
       </ThemeProvider>
--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
 export default function Dashboard() {
+    localStorage.getItem("user", JSON.stringify(response.data));
+
   return (
     <>
-        <h1>Hello</h1> 
+        <h1>Welcome To NexTalk</h1>
+        <h2>Hello, {user ? user.name : "Guest"}!</h2> 
     </>
   )
 }
