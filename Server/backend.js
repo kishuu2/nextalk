56e03d26e3fe4ca66c1817993e339315759e5ad4
diamond <chokwalakishan@gmail.com>
2025-05-23 21:08:11 +0530

    Update code

--- Server/backend.js
+++ Server/backend.js
@@ -45,7 +45,7 @@ app.use(
         },
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders: ["Content-Type"],
+        allowedHeaders: ["Content-Type", "Authorization"],
     })
 );
 
