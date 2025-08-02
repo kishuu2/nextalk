3b95acf75460b552f867202ca78fcea2ae116784
diamond <chokwalakishan@gmail.com>
2025-08-02 16:26:12 +0530

    Initial commit for Nextalk

--- pages/Components/MobileChatView.js
+++ pages/Components/MobileChatView.js
@@ -320,12 +320,11 @@ const MobileChatView = ({
                         <div style={{ flex: 1 }}>
                             <h6 style={userNameStyle}>{selectedUser?.name || 'User'}</h6>
                             <small style={userStatusStyle}>
-                                {typeof formatLastSeen === 'function' &&
-                                    formatLastSeen(
-                                        new Date(selectedUser?.lastSeen || Date.now()),
-                                        onlineUsers?.has?.(selectedUser?._id) || false
-                                    )
-                                }
+                                {formatLastSeen(
+                                    new Date(selectedUser?.lastSeen || Date.now()),
+                                    onlineUsers?.has?.(selectedUser?._id) ?? false
+                                )}
+
                             </small>
                         </div>
                     </div>
